1dede87e0f40bedd47d4c3e770179f6c2ce46e57
while-basic <c_celaya@icloud.com>
2024-12-17 23:46:25 -0700

    removed unecessary import

--- src/lib/store-logger.ts
+++ src/lib/store-logger.ts
@@ -16,7 +16,6 @@
 
 import { create } from "zustand";
 import { StreamingLog } from "../multimodal-live-types";
-import { mockLogs } from "../components/logger/mock-logs";
 
 interface StoreLoggerState {
   maxLogs: number;
